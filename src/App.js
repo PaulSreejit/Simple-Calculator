0996b083e9b5fd4b01ed92b2a6269e8621c2f2a6
Sreejit Paul <srp5121996.sreejit@gmail.com>
2022-05-17 10:44:20 +0530

    v1.0 Basic calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  let [res, setRes] = useState("");
+  const [isActive, setIsActive] = useState(false);
+
+  const num1ChangeHandler = (event) => {
+    console.log(event.target.value);
+    setNum1(event.target.value);
+  };
+
+  const num2ChangeHandler = (event) => {
+    console.log(event.target.value);
+    setNum2(event.target.value);
+  };
+
+  const addBtnHandler = () => {
+    console.log("Add button has been clicked");
+    console.log(num1, num2);
+    res = parseInt(num1) + parseInt(num2);
+    setRes(res);
+    setIsActive(!isActive);
+    console.log(res);
+  };
+  const subBtnHandler = () => {
+    console.log("Subtract button has been clicked");
+    console.log(num1, num2);
+    res = parseInt(num1) - parseInt(num2);
+    setRes(res);
+    setIsActive(!isActive);
+    console.log(res);
+  };
+  const mulBtnHandler = () => {
+    console.log("Multiply button has been clicked");
+    console.log(num1, num2);
+    res = parseInt(num1) * parseInt(num2);
+    setRes(res);
+    setIsActive(!isActive);
+    console.log(res);
+  };
+  const divBtnHandler = () => {
+    console.log("Divide button has been clicked");
+    console.log(num1, num2);
+    res = parseInt(num1) / parseInt(num2);
+    setRes(res);
+    setIsActive(!isActive);
+    console.log(res);
+  };
+
+  const Results = () => {
+    return <div>Result:{res}</div>;
+  };
+
+  const clearBtnHandler = () => {
+    console.log("Clear Button has been clicked");
+    console.log(num1, num2);
+    setNum1("");
+    setNum2("");
+    setRes("");
+    setIsActive(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Basic Calculator</h1>
+      <label>
+        <input
+          type="number"
+          name="num1"
+          value={num1}
+          onChange={num1ChangeHandler}
+        ></input>
+      </label>
+      <label>
+        <input
+          type="number"
+          name="num2"
+          value={num2}
+          onChange={num2ChangeHandler}
+        ></input>
+      </label>
+      <div>
+        <button className="operation-btn" onClick={addBtnHandler}>
+          +
+        </button>
+        <button className="operation-btn" onClick={subBtnHandler}>
+          -
+        </button>
+        <button className="operation-btn" onClick={mulBtnHandler}>
+          *
+        </button>
+        <button className="operation-btn" onClick={divBtnHandler}>
+          /
+        </button>
+      </div>
+      <div>{isActive ? <Results /> : null}</div>
+      <button onClick={clearBtnHandler}>Clear</button>
     </div>
   );
-}
+};
 
 export default App;
